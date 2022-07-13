5150862317b70d672ae710f6388e045ce8d99575
mackawara <mkawara@outlook.com>
2022-07-13 18:36:51 +0200

    feat : added Regex uisng imprt re

--- wordle.py
+++ wordle.py
@@ -1,3 +1,4 @@
+import re
 print("Pleae enter your age:")
 age=int(input("age in years : "))
 fav_car=input("what is your fav car: ")
@@ -7,8 +8,9 @@ people={"mac":{"name":"Macdonald Kawara","age":35}}
 if age>people["mac"]["age"]:
     print("You are getting old my guy")
     for car in cars:
-        if fav_car==car:
+        matches=re.findall(car,fav_car)
+        if len(matches)>0:
             print("congratulations we have a car for you")
         else:
-            print("no car for you my guy")
+            print(matches)
 
