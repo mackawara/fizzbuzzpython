5ee6f7d5ca1acd272f540e5971db21103d579495
mackawara <mkawara@outlook.com>
2022-07-13 18:43:41 +0200

    feat : created arange and itretd over it

--- wordle.py
+++ wordle.py
@@ -4,7 +4,7 @@ age=int(input("age in years : "))
 fav_car=input("what is your fav car: ")
 cars=["mazda","landrover","Jeep", "Ford", "Toyota"]
 people={"mac":{"name":"Macdonald Kawara","age":35}}
-
+""" 
 if age>people["mac"]["age"]:
     print("You are getting old my guy")
     for car in cars:
@@ -14,9 +14,12 @@ if age>people["mac"]["age"]:
         else:
             print(matches)
 
-print(people)
+print(people) """
 people.update({"mac2":{"name":"Mac2"}})
 print(people)
 
+data_range=range(len(cars))
+for data in data_range:
+    print(data)
 
 
