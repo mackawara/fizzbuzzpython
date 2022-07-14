49488ba3309251fe170ae368625ad22914c49d43
mackawara <mkawara@outlook.com>
2022-07-14 07:26:47 +0200

    got rock paper scissors MVP

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,31 +1,34 @@
 import random
-rock="rock"
-paper="paper"
-scissors="scissors"
+rock = "rock"
+paper = "paper"
+scissors = "scissors"
 
-game_var=[rock,paper,scissors]
+game_var = [rock, paper, scissors]
 while True:
-    userInput=(input("""make your choice here
+    print("in loop")
+
+    while True:
+        userInput = (input("""make your choice here
         1. Rock
         2. Paper
         3. Scissors
 
         Write number corresponding to your choice """))
+        print(type(userInput))
+        if userInput in (1, 2, 3):
+        # print("please enter a valid number between 1and 3")
+            break
 
-    while not userInput in ("1","2","3"):
-        print("please enter a valid number between 1and 3")
-        break
-    u_sel=game_var[int(userInput)-1]
-    c_sel=game_var[random.randrange(0,3)]
+    u_sel = game_var[int(userInput)-1]
+    c_sel = game_var[random.randrange(0, 3)]
     print(u_sel)
     print(c_sel)
 
-    if (u_sel==rock and c_sel==paper) or (u_sel==paper and c_sel==scissors) or (u_sel==scissors and c_sel==rock):
-        print("You lose: computer selected "+ c_sel +" and you selected" + u_sel)
-    elif u_sel==c_sel:
-        print("Tie :computer selected "+ c_sel +" and you selected " + u_sel)
+    if (u_sel == rock and c_sel == paper) or (u_sel == paper and c_sel == scissors) or (u_sel == scissors and c_sel == rock):
+        print("You lose: computer selected " +
+              c_sel + " and you selected" + u_sel)
+    elif u_sel == c_sel:
+        print("Tie :computer selected " + c_sel + " and you selected " + u_sel)
     else:
         print("congratulations you won")
     break
-
-        
