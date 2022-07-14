3f77bba2ae7a8b7da5f11ecc92b6990724dbab9e
mackawara <mkawara@outlook.com>
2022-07-14 06:12:54 +0200

    feat : refactored the code

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -4,26 +4,28 @@ paper="paper"
 scissors="scissors"
 
 game_var=[rock,paper,scissors]
-userInput=(input("""make your choice here
-1. Rock
-2. Paper
-3. Scissors
+while True:
+    userInput=(input("""make your choice here
+        1. Rock
+        2. Paper
+        3. Scissors
 
-Write number corresponding to your """))
+        Write number corresponding to your choice """))
 
-while not int(userInput) in range(1,4):
-    print("you have chosen invalid number")
+    while not userInput in ("1","2","3"):
+        print("please enter a valid number between 1and 3")
+        break
+    u_sel=game_var[int(userInput)-1]
+    c_sel=game_var[random.randrange(0,3)]
+    print(u_sel)
+    print(c_sel)
 
-if not userInput.isnumeric():
-    print("please ente a valid number between 1and 3")
-u_sel=game_var[int(userInput)-1]
-c_sel=game_var[random.randrange(0,3)]
-print(u_sel)
-print(c_sel)
+    if (u_sel==rock and c_sel==paper) or (u_sel==paper and c_sel==scissors) or (u_sel==scissors and c_sel==rock):
+        print("You lose: computer selected "+ c_sel +" and you selected" + u_sel)
+    elif u_sel==c_sel:
+        print("Tie :computer selected "+ c_sel +" and you selected " + u_sel)
+    else:
+        print("congratulations you won")
+    break
 
-if (u_sel==rock and c_sel==paper) or (u_sel==paper and c_sel==scissors) or (u_sel==scissors and c_sel==rock):
-    print("You lose computer selected "+ c_sel +" and you selected" + u_sel)
-elif u_sel==c_sel:
-    print("Tie :computer selected "+ c_sel +" and you selected " + u_sel)
-else:
-    print("congratulations you won")
+        
