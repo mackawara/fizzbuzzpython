1c4e64a5953a162a32d2b8df55e5ffde2d5b4573
mackawara <mkawara@outlook.com>
2022-07-25 19:28:34 +0200

    feat added Super function

--- oop.py
+++ oop.py
@@ -1,18 +1,25 @@
-print(" Tests fr ML")
+
 class TestObj:
-    def __init__(self, name,location):
-        self.name=name
-        self.location=location
+    def __init__(self, name, location):
+        self.name = name
+        self.location = location
 
-        self.attributes=print("I am  "+self.name+" I am from "+ location) 
+    def attributes(self):
+        print("I am  "+self.name+" I am from " + self.location)
+    height = 25
+
+
 class OBj(TestObj):
-    def __init__(self,name,location,age,attributes):
-        self.age=age
-        super().__init__(attributes)
-                     
-        
+    def __init__(self, name, location, age):
+        self.age = age
+        super().__init__(name, location)
 
-mac =TestObj("Mac","Zimbabwe")  
-marion=OBj("Marion "," Zimbabwe",21)
-mac.attributes   
-marion.attributes  
+    def age_print(self):
+        print(" My height is " + str(self.height))
+
+
+mac = TestObj("Mac", "Zimbabwe")
+marion = OBj("Marion ", " Zimbabwe", 21)
+mac.attributes()
+marion.height
+marion.age_print()
